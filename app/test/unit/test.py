d523fef62151176d96a729a4e4bd9ca69e9c8431
vanessakovalsky <v.david@kovalibre.com>
2023-01-19 17:59:57 +0100

    Add runner for report HTML

--- app/test/unit/test.py
+++ app/test/unit/test.py
@@ -1,18 +1,19 @@
 import unittest
 import json
+import HtmlTestRunner
 
 from application.application import Application
 from machine.machine import Machine
 
 class TestApplication(unittest.TestCase):
     # Initialisation de données pour mes tests
-    def setUp(self):
-        self.demo_donnes=('ma_super_liste',1)
+    # def setUp(self):
+    #     self.demo_donnes=('ma_super_liste',1)
     
-    # Nettoyage après les tests
-    def tearDown(self) -> None:
-        print('Je nettoie les données')
-        return super().tearDown()
+    # # Nettoyage après les tests
+    # def tearDown(self) -> None:
+    #     print('Je nettoie les données')
+    #     return super().tearDown()
     
 
     # Exemple de test avec un echec (failure)
@@ -25,21 +26,28 @@ class TestApplication(unittest.TestCase):
         self.assertEqual(contenu_liste,'Liste des applications')
 
     def test_add_application(self):
-        add_app = Application.add(self.demo_donnes)
+        add_app = Application.add(('ma_super_liste',1))
         self.assertEqual(add_app,'Application ajoutée')
 
-class TestMachines(unittest.TestCase):
-    def test_ajout_machine(self):
-        Machine.fichier="machines.json"
-        ajout_machine = Machine.add(Machine,'{"donnees":"mes donnees à écrire"}')
-        with open('machines.json', 'r') as f:
-            try:
-                contenu_fichier = json.load(f)
-            except Exception as e:
-                print("Exception raised | %s " % str(e))
-                exit()
-        self.assertEqual(contenu_fichier,{"donnees":"mes donnees à écrire"})
+# class TestMachines(unittest.TestCase):
+#     def test_ajout_machine(self):
+#         Machine.fichier="machines.json"
+#         ajout_machine = Machine.add(Machine,'{"donnees":"mes donnees à écrire"}')
+#         with open('machines.json', 'r') as f:
+#             try:
+#                 contenu_fichier = json.load(f)
+#             except Exception as e:
+#                 print("Exception raised | %s " % str(e))
+#                 exit()
+#         self.assertEqual(contenu_fichier,{"donnees":"mes donnees à écrire"})
 
-    
-if __name__ == '__main__':
-	unittest.main()
+
+
+test1 = unittest.TestLoader().loadTestsFromTestCase(TestApplication)
+suite = unittest.TestSuite([test1])
+runner = HtmlTestRunner.HTMLTestRunner(verbosity=2, output='report', report_name='report')
+runner.run(suite)
+
+
+if __name__ == "__main__":
+    unittest.main()
